76ee7cbaa5846701a61d6c8b85c97500046f8a2f
vivektiwary1992 <77327606+vivektiwary1992@users.noreply.github.com>
2021-07-21 16:14:45 +0530

    bash file

--- packege.sh
+++ packege.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
-sudo systemctl start docker
+sudo apt-get remove docker docker-engine docker.io -y
+sudo apt install docker.io -y
+sudo systemctl start docker -y
 sudo systemctl enable docker
 wget https://vtvivek713.s3.us-east-2.amazonaws.com/webapp.war
 wget https://vtvivek713.s3.us-east-2.amazonaws.com/Dockerfile
 sudo docker build -t customimage .
-sudo docker run -d -p 8080:8080 --name coustomcoentener coustomimage
+sudo docker run -d -p 8080:8080 --name coustomcoentener customimage /bin/bash
